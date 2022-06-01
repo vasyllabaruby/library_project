3e95847cd82dad5c3c30211bc7aac3663c183f3e
Vasyl <vasyl.laba@rubygarage.org>
2022-06-01 11:14:31 +0300

    Refactor ValidationService to module because it has no data

--- lib/services/validation_service.rb
+++ lib/services/validation_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ValidationService
+module ValidationService
   def self.not_blank(*str)
     str.each do |el|
       return false if !el.is_a?(String) || el.strip.empty?

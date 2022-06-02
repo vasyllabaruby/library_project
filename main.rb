8ca92e68f43f94d4fbc500a0ec6a27c5cc36ac32
Vasyl <vasyl.laba@rubygarage.org>
2022-06-02 11:21:09 +0300

    refactor main.rb

--- main.rb
+++ main.rb
@@ -2,36 +2,42 @@ require 'entities/library'
 
 my_library = Library.new
 
-my_library.add_author('Jim', nil)
-my_library.add_author('Oleg', 'Greeter')
+author_jim = Author.new('Jim')
+author_oleg = Author.new('Oleg', 'Greeter')
+my_library.add_author(author_jim)
+my_library.add_author(author_oleg)
 
-author1 = my_library.authors[1]
-author2 = my_library.authors[3]
-
-my_library.add_book('Rain', author1)
-my_library.add_book('Pain', author2)
-my_library.add_book('The Great Gatsby', author2)
+book_rain = Book.new('Rain', author_jim)
+book_pain = Book.new('Pain', author_oleg)
+book_gatsby = Book.new('The Great Gatsby', author_oleg)
+my_library.add_book(book_rain)
+my_library.add_book(book_pain)
+my_library.add_book(book_gatsby)
 # my_library.add_book(nil, author2)
 
-my_library.add_reader('Mykola', 'kol12@mai.com', 'Chernivtsi', 'Main', 1)
-my_library.add_reader('Ivan', 'iv@mai.com', 'Kyiv', 'Kaspruka', 5)
-my_library.add_reader('Andrew', 'aaBB@mai.com', 'Lviv', 'Main', 31)
+reader_mykola = Reader.new('Mykola', 'kol12@mai.com', 'Chernivtsi', 'Main', 1)
+reader_ivan = Reader.new('Ivan', 'iv@mai.com', 'Kyiv', 'Kaspruka', 5)
+reader_andrew = Reader.new('Andrew', 'aaBB@mai.com', 'Lviv', 'Main', 31)
+my_library.add_reader(reader_mykola)
+my_library.add_reader(reader_ivan)
+my_library.add_reader(reader_andrew)
 
-book_pain = my_library.books[1]
-book_great = my_library.books[2]
-book_rain = my_library.books[0]
+order_p_m = Order.new(book_pain, reader_mykola)
+order_g_m = Order.new(book_gatsby, reader_mykola)
+order_r_m = Order.new(book_rain, reader_mykola)
+order_g_i = Order.new(book_gatsby, reader_ivan)
+order_p_a = Order.new(book_pain, reader_andrew)
+order_p_i = Order.new(book_pain, reader_ivan)
+order_r_a = Order.new(book_rain, reader_andrew)
 
-reader1_mykola = my_library.readers[0]
-reader2_ivan = my_library.readers[1]
-reader2_andrew = my_library.readers[2]
 
-my_library.add_order(book_pain, reader1_mykola)
-my_library.add_order(book_great, reader1_mykola)
-my_library.add_order(book_great, reader2_ivan)
-my_library.add_order(book_great, reader2_ivan)
-my_library.add_order(book_pain, reader2_andrew)
-my_library.add_order(book_pain, reader2_ivan)
-my_library.add_order(book_rain, reader2_ivan)
+my_library.add_order(order_p_m)
+my_library.add_order(order_g_m)
+my_library.add_order(order_r_m)
+my_library.add_order(order_g_i)
+my_library.add_order(order_p_a)
+my_library.add_order(order_p_i)
+my_library.add_order(order_r_a)
 
 my_library.save_lib
 
